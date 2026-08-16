28d8a066c9e7d5cdf4595ace7a5a167a814a83b6
v-trakala <valery.trakala@gmail.com>
2026-08-16 17:19:07 +0200

    cleaned up

---
+++ News/News/Core/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by valery.trakala on 16/08/2026.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+	
+	var body: some View {
+		VStack {
+			Text("Hello")
+		}
+	}
+	
+}
+
+#Preview {
+	ContentView()
+}
--- News/News/Core/NewsApp.swift
+++ News/News/Core/NewsApp.swift
@@ -10,12 +10,10 @@ import CoreData
 
 @main
 struct NewsApp: App {
-    let persistenceController = PersistenceController.shared
-
+	
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
